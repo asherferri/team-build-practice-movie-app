68950cc1ebcc26a9e2c662ffc853c1a0a61abc81
Damion Wiltshire <drw.5452@gmail.com>
2020-08-27 14:45:56 -0700

    Adding remaining changes

--- movie-app/controllers/movies-controller.js
+++ movie-app/controllers/movies-controller.js
@@ -13,6 +13,63 @@ const MovieController = {
             })
             .catch(next)
     }
+
+    show: (req, res, next) => {
+        Movie.getById(req.params.id)
+            .then((movie) => {
+                res.json({
+                    message: 'ok',
+                    data: { movie },
+                });
+            })
+            .catch(next);
+    };
+
+    create: (req, res, next) => {
+        new Movie({
+            title: req.body.title,
+            description: req.body.description,
+            genre: req.body.genre,
+            user_id: req.user.id,
+        })
+            .save()
+            .then((movie) => {
+                res.json({
+                    message: 'Movie added successfully!',
+                    data: { movie },
+                });
+            })
+            .catch(next);
+    };
+
+    update: (req, res, next) => {
+        Movie.getById(req.params.id)
+            .then((movie) =>
+                movie.update({
+                    title: req.body.title,
+                    description: req.body.description,
+                    genre: req.body.genre,
+                })
+            )
+            .then((movie) => {
+                res.json({
+                    message: 'Movie updated successfully!',
+                    data: { movie },
+                });
+            })
+            .catch(next);
+    };
+
+    delete: (req, res, next) => {
+        Movie.getById(req.params.id)
+            .then((movie) => movie.delete())
+            .then(() => {
+                res.json({
+                    message: 'Movie deleted successfully!',
+                });
+            })
+            .catch(next);
+    };
 }
 
 module.exports = MovieController
