414d3e93c33467d87b07de1b9241e5b5874ba60d
Damion Wiltshire <drw.5452@gmail.com>
2020-08-27 13:13:27 -0700

    getAll implemented

---
+++ movie-app/models/Movie.js
@@ -0,0 +1,18 @@
+const db = require('../db/config')
+
+class Movie {
+    constructor({ id, title, description, genre, user_id }) {
+        this.id = id || null
+        this.title = title
+        this.description = description
+        this.genre = genre
+        this.user_id =  user_id
+    }
+
+    static getAll() {
+        return db.manyOrNone(
+            'SELECT * FROM movies ORDER BY id DESC'
+        )
+        .then(movies => movies.map(movie => new this(movie)))
+    }
+}
