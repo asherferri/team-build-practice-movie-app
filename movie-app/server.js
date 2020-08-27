a53b664b81d215fbe4f22176b1927f5dbe564a53
Asher Ferri <asher@Ashers-MacBook-Pro.local>
2020-08-27 17:30:56 -0400

    Login.jsx Register.jsx writeten

--- movie-app/server.js
+++ movie-app/server.js
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 const authRoutes = require('./routes/auth-routes')
 app.use('/api/auth', authRoutes)
 const movieRoutes = require('./routes/movie-routes')
-app.use('./api/movies', movieRoutes)
+app.use('/api/movies', movieRoutes)
 
 //error handlers
 app.use('*', (req, res) => {
